3ce1e4fe171de051cba2bd118fec1adc99f7729b
streaper2 <streaper2@gmail.com>
2021-11-03 10:21:54 +0100

    Delete lib/themes directory

--- lib/themes/CustomButtonStyle.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomButtonStyle extends ElevatedButton {
-  ButtonStyle Build(context) {
-    return ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.black87));
-  }
-}
